80bd44ef90262b5e109a83cc326039e0f91bdd9b
castromir <141286212+castromir@users.noreply.github.com>
2024-12-02 19:18:57 -0300

    Delete projetoCJ directory

--- projetoCJ/src/main/java/com/projetoCJ/repositories/AgendaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetoCJ.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projetoCJ.Agenda;
-
-public interface AgendaRepository extends JpaRepository<Agenda, Long> {
-}
--- projetoCJ/src/main/java/com/projetoCJ/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetoCJ.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projetoCJ.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
