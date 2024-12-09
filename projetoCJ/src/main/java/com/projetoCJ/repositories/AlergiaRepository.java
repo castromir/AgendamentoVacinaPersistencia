427929b6de954581cc83dd82b8bac0f98366b0a0
castromir <141286212+castromir@users.noreply.github.com>
2024-12-08 23:59:20 -0300

    Delete projetoCJ directory

--- projetoCJ/src/main/java/com/projetoCJ/repositories/AlergiaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetoCJ.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projetoCJ.Alergia;
-
-public interface AlergiaRepository extends JpaRepository<Alergia, Long> {
-}
--- projetoCJ/src/main/java/com/projetoCJ/repositories/VacinaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetoCJ.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.projetoCJ.Vacina;
-
-public interface VacinaRepository extends JpaRepository<Vacina, Long> {
-}
